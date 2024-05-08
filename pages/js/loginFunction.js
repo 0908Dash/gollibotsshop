fe838fc6d57db21c0830120f7a46909700f478f6
Randall Enfinger <randydash304@gmail.com>
2024-05-08 12:06:29 -0500

    Update loginFunction.js

--- pages/js/loginFunction.js
+++ pages/js/loginFunction.js
@@ -2,7 +2,7 @@
 
 function loginWithDiscord() {
     // Redirect to Discord OAuth login page
-    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1156597666680160376&response_type=code&redirect_uri=https%3A%2F%2F3nx3l3gk-1118.use2.devtunnels.ms%2F&scope=identify+guilds+email+guilds.members.read+connections+openid';
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1156597666680160376&response_type=code&redirect_uri=https%3A%2F%2F3nx3l3gk-1118.use2.devtunnels.ms%2Fauth%2Fdiscord%2Fcallback&scope=identify+guilds+email+guilds.members.read+connections+openid';
 }
 
 // Add event listener to traditional login form
