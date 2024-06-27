0212c61198c5bc647e914c2c57de3d565f28da78
Randall Enfinger <randydash304@gmail.com>
2024-06-26 21:47:02 -0500

    Update loginFunction.js

--- pages/js/loginFunction.js
+++ pages/js/loginFunction.js
@@ -1,19 +1,39 @@
 // loginFunction.js
 
-function loginWithDiscord() {
-    // Redirect to Discord OAuth login page
-    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1156597666680160376&response_type=code&redirect_uri=https%3A%2F%2F3nx3l3gk-1118.use2.devtunnels.ms%2Fauth%2Fdiscord%2Fcallback&scope=identify+guilds+email+guilds.members.read+connections+openid';
-}
-
-// Add event listener to traditional login form
-document.getElementById('traditionalLoginForm').addEventListener('submit', function (event) {
+// Function to handle traditional login form submission
+document.getElementById('LoginForm').addEventListener('submit', async function (event) {
     event.preventDefault();
-    // Implement logic for traditional login (send data to your server)
-    const formData = new FormData(event.target);
-    const username = formData.get('username');
-    const password = formData.get('password');
-    // Send data to your server for authentication
 
-    fetch('/login') // send username and password to server for auth checking 
+    const formData = new FormData(this);
+    const emailOrUsername = formData.get('emailOrUsername');
+    const password = formData.get('password');
+
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ emailOrUsername, password })
+        });
+
+        if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(errorMessage);
+        }
+
+        const result = await response.text();
+        console.log(result); // For debugging, you can replace this with appropriate UI handling
+        alert('Login successful'); // Example: Display a success message
+        window.location.href = '/dashboard'; // Redirect to dashboard or another page
+    } catch (error) {
+        console.error('Error during login:', error);
+        alert('Failed to login. Please check your credentials and try again.');
+    }
 });
 
+// Function to initiate OAuth login with Discord
+function loginWithDiscord() {
+    // Redirect to Discord OAuth login page
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1156597666680160376&response_type=code&redirect_uri=https%3A%2F%2Fgollibot.gollicraft.com%2Fcallback&scope=email+identify+connections+guilds';
+}
